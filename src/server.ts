a8b4431ef2323d6f39a54cd07b66f91f29a83ecf
Vakhurin Sergey <igelbox@gmail.com>
2017-08-11 01:35:49 +0300

    added swagger and tsoa

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,34 @@
-import { createServer } from 'http';
+import './controllers/test';
 
-createServer()
-  .listen(8080, (error) => {
-    if (error) {
-      console.error('Cannot start server', error);
-      return;
-    }
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import { RegisterRoutes } from './routes';
 
-    console.log('Server is running');
+import swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../dist/swagger.json');
+
+const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+RegisterRoutes(app);
+
+app.use((err, req, res, next) => {
+  res.status(400).json({
+    message: err.message || 'Internal error',
+    type: err && err.constructor && err.constructor.name,
+    data: err.fields,
   });
+});
+
+console.log('Starting server ...');
+app.listen(8080, (error) => {
+  if (error) {
+    console.error('Cannot start server', error);
+    return;
+  }
+  console.log('Server is running');
+});
